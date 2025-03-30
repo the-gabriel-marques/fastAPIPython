23a38867f75e3957ad4c1dc5c901cef1a3af6f90
the-gabriel-marques <gabrielmarquesdasilvacontato@gmail.com>
2025-03-30 14:28:08 -0300

    env incluido

--- database/db.py
+++ database/db.py
@@ -1,4 +1,24 @@
-import sqlite3
+import oracledb
+import os
+from dotenv import load_dotenv
+
+# Carregar variáveis do .env
+load_dotenv()
+
+# Pegar credenciais do ambiente
+HOST = os.getenv("ORACLE_HOST")
+PORT = os.getenv("ORACLE_PORT")
+SERVICE_NAME = os.getenv("ORACLE_SERVICE_NAME")
+USER = os.getenv("ORACLE_USER")
+PASSWORD = os.getenv("ORACLE_PASSWORD")
+
+# Criar string de conexão
+dsn = f"{HOST}:{PORT}/{SERVICE_NAME}"
 
 def conectar():
-    return sqlite3.connect("banco.db")
+    try:
+        conn = oracledb.connect(user=USER, password=PASSWORD, dsn=dsn)
+        return conn
+    except oracledb.DatabaseError as e:
+        print("Erro ao conectar ao Oracle:", e)
+        return None
